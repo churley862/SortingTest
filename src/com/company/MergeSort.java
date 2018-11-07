b7791337ed23936a66a5b7ddf1390e6bab3e4e91
Patrick Hurley <phurley@gmail.com>
2018-11-07 16:28:46 -0500

    Fix merge sort merge

--- src/com/company/MergeSort.java
+++ src/com/company/MergeSort.java
@@ -7,48 +7,53 @@ public class MergeSort {
         int[] temp = new int[dataSize];
         int leftStart = 0;
         int rightEnd = dataSize-1;
-        sort(leftStart, rightEnd,temp, data);
-        System.out.println(temp[0] +" " +data[0]);
+        sort(leftStart, rightEnd, temp, data);
+
         return new SortMetrics(swaps,comparisons);
     }
 
-    private static void sort(int leftStart, int rightEnd,int[] temp, int[] data) {
-        if(leftStart >= rightEnd) {
-            return;
+    private static void sort(int leftStart, int rightEnd, int[] temp, int[] data) {
+        if(leftStart < rightEnd) {
+            int middle = (leftStart + rightEnd) / 2;
+            sort(leftStart, middle, temp, data);
+            sort(middle+1, rightEnd, temp, data);
+            mergeSides(leftStart, rightEnd, temp, data);
         }
-        int middle = (leftStart + rightEnd)/2;
-        sort(leftStart,middle,temp,data);
-        sort(middle+1,rightEnd,temp,data);
-        mergeSides(leftStart,rightEnd,temp,data);
+    }
 
-        }
-
-    private static void mergeSides(int leftStart, int rightEnd,int[] temp , int[] data) {
+    private static void mergeSides(int leftStart, int rightEnd, int[] temp , int[] data) {
         int leftEnd = (rightEnd+leftStart)/2;
-        int rightStart = leftEnd+1;
+        int rightStart = leftEnd + 1;
         int left = leftStart;
         int right = rightStart;
         int index = leftStart;
-        while (left <= leftEnd && right <= rightEnd){
+
+        while (left <= leftEnd && right <= rightEnd) {
             if (data[left] <= data[right]){
-                temp [index] = data[left];
+                temp[index] = data[left];
                 left++;
-            }else{
+            } else {
                 temp[index] = data[right];
                 right++;
             }
             index++;
         }
+
         while(right <= rightEnd){
             temp[index] = data[right];
             right++;
+            index++;
         }
+
         while (left <= leftEnd){
             temp[index] = data[left];
             left++;
+            index++;
         }
-        data = temp.clone();
 
+        for (index = leftStart; index <= rightEnd; ++index) {
+            data[index] = temp[index];
+        }
     }
 }
 
